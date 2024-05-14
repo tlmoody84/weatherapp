840e3942ad285fbda499520f249d5eb38e51a94d
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-05-13 22:33:24 -0400

    Update script.js
    Fetched all id's to assign a variable to use in javascript

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let apiKey = '3ae7d10fc4bceaf4a69f018ef7340781';
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
+let locationInput = document.getElementById('locationInput');
+let temperatureElement = document.getElementById('temperature');
+let searchButton = document.getElementById('searchButton');
+let locationElement = document.getElementById('location');
+let descriptionElement = document.getElementById('description');
+let windElement = document.getElementById('speed');
+let gustElement = document.getElementById('gust');
+
+searchButton.addEventListener('click', () => {
+  let location = locationInput.value;
+  if (location) {
+    fetchWeather(location);
+  }
+});
